7acf6b952ba37030ed2d17100577cce1e452b35d
vadiminshakov <vadiminshakov@gmail.com>
2020-02-12 12:21:59 +0300

    add script args

---
+++ sign.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ $2 = "commit" ]; then
+  docker exec $1 peer channel update -f wrapped.pb -c $CHANNEL_NAME -o orderer.example.com:7050 --tls --cafile $ORDERER_CA
+else
+  docker cp wrappedDelta.pb $1:/opt/gopath/src/github.com/hyperledger/fabric/peer/wrapped.pb
+  docker exec $1 /bin/sh -c "CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE  peer channel signconfigtx -f wrapped.pb"
+  docker cp $1:/opt/gopath/src/github.com/hyperledger/fabric/peer/wrapped.pb wrappedDelta.pb
+fi
