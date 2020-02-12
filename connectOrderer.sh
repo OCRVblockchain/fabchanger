c5fba8191e03c611c8e66932c36f4c987d878854
vadiminshakov <vadiminshakov@gmail.com>
2020-02-12 11:07:33 +0300

    fix script args

--- connectOrderer.sh
+++ connectOrderer.sh
@@ -5,8 +5,8 @@ mkdir artifacts
 ./fabchanger --mode getblock -o ./artifacts/configOrderer.json
 ./fabchanger --mode configtxtojson -o ./artifacts/extendOrderer.json --join orderer
 ./fabchanger --mode merge --f ./artifacts/configOrderer.json --merge ./artifacts/extendOrderer.json --o ./artifacts/merged.json --join orderer
-./fabchanger --mode jsontoproto --f ./artifacts/configOrderer.json --o ./artifacts/configOrderer.pb --join orderer
-./fabchanger --mode jsontoproto --f ./artifacts/merged.json --o ./artifacts/merged.pb --join orderer
+./fabchanger --mode jsontoproto --f ./artifacts/configOrderer.json --o ./artifacts/configOrderer.pb
+./fabchanger --mode jsontoproto --f ./artifacts/merged.json --o ./artifacts/merged.pb
 ./fabchanger --mode delta -f ./artifacts/configOrderer.pb -comparewith ./artifacts/merged.pb -o ./artifacts/delta.pb
 ./fabchanger --mode wrap -f ./artifacts/delta.pb -o wrappedDelta.pb
 rm -rf ./artifacts/
