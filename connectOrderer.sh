4942b5ccf410b297df93818f401f795f78d9cc79
vadiminshakov <vadiminshakov@gmail.com>
2020-02-12 11:05:01 +0300

    add artifacts dir and purge

--- connectOrderer.sh
+++ connectOrderer.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 go build -o fabchanger cmd/main.go
-./fabchanger --mode getblock -o configOrderer.json
-./fabchanger --mode configtxtojson -o extendOrderer.json --join orderer
-./fabchanger --mode merge --f configOrderer.json --merge extendOrderer.json --o merged.json --join orderer
-./fabchanger --mode jsontoproto --f configOrderer.json --o configOrderer.pb --join orderer
-./fabchanger --mode jsontoproto --f merged.json --o merged.pb --join orderer
-./fabchanger --mode delta -f configOrderer.pb -comparewith merged.pb -o delta.pb
-./fabchanger --mode wrap -f delta.pb -o wrappedDelta.pb
+mkdir artifacts
+./fabchanger --mode getblock -o ./artifacts/configOrderer.json
+./fabchanger --mode configtxtojson -o ./artifacts/extendOrderer.json --join orderer
+./fabchanger --mode merge --f ./artifacts/configOrderer.json --merge ./artifacts/extendOrderer.json --o ./artifacts/merged.json --join orderer
+./fabchanger --mode jsontoproto --f ./artifacts/configOrderer.json --o ./artifacts/configOrderer.pb --join orderer
+./fabchanger --mode jsontoproto --f ./artifacts/merged.json --o ./artifacts/merged.pb --join orderer
+./fabchanger --mode delta -f ./artifacts/configOrderer.pb -comparewith ./artifacts/merged.pb -o ./artifacts/delta.pb
+./fabchanger --mode wrap -f ./artifacts/delta.pb -o wrappedDelta.pb
+rm -rf ./artifacts/
+
 
