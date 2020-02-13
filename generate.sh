7bf64e259e4fc5fe18524bfd457dd8753d70453d
vadiminshakov <vadiminshakov@gmail.com>
2020-02-13 11:18:54 +0300

    script for generating crypto materials added

---
+++ generate.sh
@@ -0,0 +1,10 @@
+which cryptogen
+  if [ "$?" -ne 0 ]; then
+    echo "cryptogen tool not found. exiting"
+    exit 1
+  fi
+  if [ -d "crypto-config" ]; then
+    rm -Rf crypto-config
+  fi
+  set -x
+  cryptogen generate --config=./crypto-config.yaml
