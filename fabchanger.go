d729e30c158c7241eb22e09d1cd2086c5b5029ad
vadiminshakov <vadiminshakov@gmail.com>
2020-02-17 18:25:09 +0300

    add signing functionality

--- fabchanger.go
+++ fabchanger.go
@@ -2,6 +2,7 @@ package fabchanger
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"github.com/OCRVblockchain/fabchanger/config"
@@ -9,24 +10,24 @@ import (
 	"github.com/OCRVblockchain/fabchanger/configtxgen/genesisconfig"
 	"github.com/OCRVblockchain/fabchanger/configtxlator/update"
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/hyperledger/fabric-protos-go/common"
 	cb "github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric-protos-go/msp"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/ledger"
-	mspclient "github.com/hyperledger/fabric-sdk-go/pkg/client/msp"
-	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/core"
 	fabricconfig "github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
-	"github.com/hyperledger/fabric/bccsp/factory"
 	"github.com/hyperledger/fabric/common/configtx"
 	"github.com/hyperledger/fabric/common/tools/protolator"
 	"github.com/hyperledger/fabric/common/tools/protolator/protoext/ordererext"
 	"github.com/hyperledger/fabric/common/util"
-	mspmgmt "github.com/hyperledger/fabric/msp/mgmt"
 	"github.com/hyperledger/fabric/protoutil"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
 	"reflect"
+	"time"
 )
 
 type FabChanger struct {
@@ -263,8 +264,6 @@ func (f *FabChanger) JSONToProtoConfig(source, newName string) error {
 
 func (f *FabChanger) ComputeDelta(original, updated, output string) error {
 
-	//var envelopeWrapper = map[string]interface{}{"payload": map[string]interface{}{"header": map[string]interface{}{"channel_header": map[string]interface{}{"channel_id": f.Config.Channel, "type": 2}}}}
-
 	originalFile, err := os.Open(original)
 	if err != nil {
 		return err
@@ -403,101 +402,214 @@ func (f *FabChanger) Wrap(channelTxFile, output string) error {
 	return nil
 }
 
-func (f *FabChanger) Sign(channelTxFile string) error {
-
+func (f *FabChanger) Sign(input, output string) error {
 	fabConfig := fabricconfig.FromFile(f.Config.General.ConnectionProfile)
+
 	sdk, err := fabsdk.New(fabConfig)
 	if err != nil {
 		return err
 	}
 	defer sdk.Close()
 
-	mspcli, err := mspclient.New(sdk.Context(), mspclient.WithOrg(f.Config.MyOrg))
+	// get CryptoSuite & SigningManager for signing
+	ClientProvider := sdk.Context(fabsdk.WithUser(f.Config.Identity), fabsdk.WithOrg(f.Config.MyOrg))
+	Client, err := ClientProvider()
 	if err != nil {
 		return err
 	}
 
-	fileData, err := ioutil.ReadFile(channelTxFile)
+	SigningManager := Client.SigningManager()
+
+	fileBytes, err := ioutil.ReadFile(input)
 	if err != nil {
 		return err
 	}
 
-	ctxEnv, err := protoutil.UnmarshalEnvelope(fileData)
+	var Envelope = &common.Envelope{}
+	err = proto.Unmarshal(fileBytes, Envelope)
+
+	payload, err := protoutil.UnmarshalPayload(Envelope.Payload)
 	if err != nil {
 		return err
-	}
-
-	signer, err := mspcli.GetSigningIdentity(f.Config.Identity)
-	if err != nil {
-		return err
-	}
-
-	sCtxEnv, err := f.sanityCheckAndSignConfigTx(ctxEnv, signer)
-	if err != nil {
-		return err
-	}
-
-	sCtxEnvData := protoutil.MarshalOrPanic(sCtxEnv)
-
-	return ioutil.WriteFile(channelTxFile, sCtxEnvData, 0660)
-}
-
-func (f *FabChanger) sanityCheckAndSignConfigTx(envConfigUpdate *cb.Envelope, signer msp.SigningIdentity) (*cb.Envelope, error) {
-
-	newsigner, err := mspmgmt.GetLocalMSP(factory.GetDefault()).GetDefaultSigningIdentity()
-	if err != nil {
-		return nil, errors.WithMessage(err, "error obtaining the default signing identity")
-	}
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("bad signer, error: %s", err))
-	}
-
-	payload, err := protoutil.UnmarshalPayload(envConfigUpdate.Payload)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("bad payload, error: %s", err))
-	}
-
-	if payload.Header == nil || payload.Header.ChannelHeader == nil {
-		return nil, errors.New(fmt.Sprintf("bad header, error: %s", err))
-	}
-
-	ch, err := protoutil.UnmarshalChannelHeader(payload.Header.ChannelHeader)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("could not unmarshall channel header, error: %s", err))
-	}
-
-	if ch.Type != int32(cb.HeaderType_CONFIG_UPDATE) {
-		return nil, errors.New("bad type")
-	}
-
-	if ch.ChannelId == "" {
-		return nil, errors.New("empty channel id")
-	}
-
-	if ch.ChannelId != f.Config.Channel {
-		return nil, errors.New(fmt.Sprintf("mismatched channel ID %s != %s", ch.ChannelId, f.Config.Channel))
 	}
 
 	configUpdateEnv, err := configtx.UnmarshalConfigUpdateEnvelope(payload.Data)
 	if err != nil {
-		return nil, errors.New("Bad config update env")
+		return err
 	}
 
-	sigHeader, err := protoutil.NewSignatureHeader(newsigner)
+	nonce, err := CreateNonce()
 	if err != nil {
-		return nil, err
+		return err
+	}
+
+	identitySerialized, err := serializeIdentity(f.Config.ClientCert, f.Config.MSPId)
+	sigHeader := &cb.SignatureHeader{
+		Creator: identitySerialized,
+		Nonce:   nonce,
 	}
 
 	configSig := &cb.ConfigSignature{
 		SignatureHeader: protoutil.MarshalOrPanic(sigHeader),
 	}
 
-	configSig.Signature, err = newsigner.Sign(util.ConcatenateBytes(configSig.SignatureHeader, configUpdateEnv.ConfigUpdate))
+	configSig.Signature, err = SigningManager.Sign(util.ConcatenateBytes(configSig.SignatureHeader, configUpdateEnv.ConfigUpdate), Client.PrivateKey())
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	configUpdateEnv.Signatures = append(configUpdateEnv.Signatures, configSig)
 
-	return protoutil.CreateSignedEnvelope(cb.HeaderType_CONFIG_UPDATE, f.Config.Channel, newsigner, configUpdateEnv, 0, 0)
+	EnvelopeSigned, err := CreateSignedEnvelope(f.Config.ClientCert, f.Config.MSPId, Client.PrivateKey(), f.Config.Identity, cb.HeaderType_CONFIG_UPDATE, f.Config.Channel, configUpdateEnv, SigningManager, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+
+	// check envelope
+	payloadData, err := protoutil.UnmarshalPayload(EnvelopeSigned.Payload)
+	if err != nil {
+		return err
+	}
+
+	if payloadData.Header == nil || payloadData.Header.ChannelHeader == nil {
+		return errors.New("bad header")
+	}
+
+	_, err = protoutil.UnmarshalChannelHeader(payloadData.Header.ChannelHeader)
+	if err != nil {
+		return err
+	}
+
+	// write to file signed and verified envelope
+	EnvelopeProto, err := proto.Marshal(EnvelopeSigned)
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(output, EnvelopeProto, 0755); err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateNonce generates a nonce using the common/crypto package.
+func CreateNonce() ([]byte, error) {
+	nonce, err := getRandomNonce()
+	return nonce, errors.WithMessage(err, "error generating random nonce")
+}
+
+func getRandomNonce() ([]byte, error) {
+	key := make([]byte, 24)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, errors.Wrap(err, "error getting random bytes")
+	}
+	return key, nil
+}
+
+func CreateSignedEnvelope(
+	ClientCert string,
+	MSPId string,
+	key core.Key,
+	identity string,
+	txType common.HeaderType,
+	channelID string,
+	dataMsg proto.Message,
+	signer core.SigningManager,
+	msgVersion int32,
+	epoch uint64,
+	tlsCertHash []byte,
+) (*common.Envelope, error) {
+	payloadChannelHeader := MakeChannelHeader(txType, msgVersion, channelID, epoch)
+	payloadChannelHeader.TlsCertHash = tlsCertHash
+	var err error
+	payloadSignatureHeader := &common.SignatureHeader{}
+
+	nonce, err := CreateNonce()
+	if err != nil {
+		return nil, err
+	}
+
+	identitySerialized, err := serializeIdentity(ClientCert, MSPId)
+	payloadSignatureHeader = &cb.SignatureHeader{
+		Creator: identitySerialized,
+		Nonce:   nonce,
+	}
+
+	payloadSignatureHeader = &cb.SignatureHeader{
+		Creator: []byte(identity),
+		Nonce:   nonce,
+	}
+
+	data, err := proto.Marshal(dataMsg)
+	if err != nil {
+		return nil, errors.Wrap(err, "error marshaling")
+	}
+
+	paylBytes := MarshalOrPanic(
+		&common.Payload{
+			Header: MakePayloadHeader(payloadChannelHeader, payloadSignatureHeader),
+			Data:   data,
+		},
+	)
+
+	var sig []byte
+	if signer != nil {
+		sig, err = signer.Sign(paylBytes, key)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	env := &common.Envelope{
+		Payload:   paylBytes,
+		Signature: sig,
+	}
+
+	return env, nil
+}
+
+// MakeChannelHeader creates a ChannelHeader.
+func MakeChannelHeader(headerType cb.HeaderType, version int32, chainID string, epoch uint64) *cb.ChannelHeader {
+	return &cb.ChannelHeader{
+		Type:    int32(headerType),
+		Version: version,
+		Timestamp: &timestamp.Timestamp{
+			Seconds: time.Now().Unix(),
+			Nanos:   0,
+		},
+		ChannelId: chainID,
+		Epoch:     epoch,
+	}
+}
+
+// MarshalOrPanic serializes a protobuf message and panics if this
+// operation fails
+func MarshalOrPanic(pb proto.Message) []byte {
+	data, err := proto.Marshal(pb)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+// MakePayloadHeader creates a Payload Header.
+func MakePayloadHeader(ch *cb.ChannelHeader, sh *cb.SignatureHeader) *cb.Header {
+	return &cb.Header{
+		ChannelHeader:   MarshalOrPanic(ch),
+		SignatureHeader: MarshalOrPanic(sh),
+	}
+}
+
+func serializeIdentity(clientCert string, mspID string) ([]byte, error) {
+	b, err := ioutil.ReadFile(clientCert)
+	if err != nil {
+		return nil, err
+	}
+	sId := &msp.SerializedIdentity{
+		Mspid:   mspID,
+		IdBytes: b,
+	}
+	return protoutil.MarshalOrPanic(sId), nil
 }
